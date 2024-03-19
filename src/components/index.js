3b3685fe0b85f6f5b46309aa2a8dc7c9fd19070a
wahyukmr <maryu9229@gmail.com>
2024-03-19 15:29:55 +0700

    [update] 🚀 updating the export file component

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import FeedPost from './FeedPost';
+import LoadContent from './LoadContent';
+import ProfileHeader from './ProfileHeader';
+ProfileHeader;
+
+export {FeedPost, LoadContent, ProfileHeader};
