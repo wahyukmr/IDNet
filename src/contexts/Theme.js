fb8189a57ece9329d3fccbad71f4caaa7929eafc
wahyukmr <maryu9229@gmail.com>
2024-03-25 11:46:04 +0700

    [update] 🚀 updating and added color theme

--- src/contexts/Theme.js
+++ src/contexts/Theme.js
@@ -1,16 +1,22 @@
 import {createContext, useContext, useEffect, useState} from 'react';
 import {useColorScheme} from 'react-native';
+import {darkTheme, lightTheme} from '../utils/colors';
 
 const ThemeContext = createContext({
-  theme: 'light',
+  theme: lightTheme,
   setTheme: () => {},
 });
 
 export const ThemeProvider = ({children}) => {
   const systemTheme = useColorScheme();
-  const [theme, setTheme] = useState(systemTheme);
+  const [theme, setTheme] = useState(
+    systemTheme === 'dark' ? darkTheme : lightTheme,
+  );
 
-  useEffect(() => setTheme(systemTheme), [systemTheme]);
+  useEffect(
+    () => setTheme(systemTheme === 'dark' ? darkTheme : lightTheme),
+    [systemTheme],
+  );
 
   return (
     <ThemeContext.Provider value={{theme, setTheme}}>
---
+++ src/utils/colors.js
@@ -0,0 +1,27 @@
+export const lightTheme = {
+  barstyle: 'dark-content',
+  primary100: '#E05297',
+  primary200: '#F3BAD6',
+  primary300: '#E0E2E5',
+  accent100: '#f5be0b',
+  accent200: '#fef1c7',
+  text100: '#292524',
+  text200: '#78716c',
+  bg100: '#FCEEF5',
+  bg200: '#ffffff',
+  border: '#FAD9E6',
+};
+
+export const darkTheme = {
+  barstyle: 'light-content',
+  primary100: '#E05297',
+  primary200: '#F3BAD6',
+  primary300: '#E0E2E5',
+  accent100: '#f5be0b',
+  accent200: '#fef1c7',
+  text100: '#FFFFFF',
+  text200: '#e0e0e0',
+  bg100: '#0F1626',
+  bg200: '#1e2436',
+  border: '#363c4e',
+};
